aea23849327235bb5d1ba1d9bc644690c0572b79
Jones-SnowCone <joelwhyte02@gmail.com>
2021-11-26 17:13:28 -0500

    Changed server listening

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
     res.sendFile(_dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+    console.log("user connected");
+    socket.on("disconnet", () => {
+        console.log("user disconneted")
+    })
+})
+
 server.listen(3000, () => {
-    console.log("listening on *:3000");
+    console.log(`listening on ${server.address().port}`);
 });
 
